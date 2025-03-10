bf963e926df3b3384d5b20707ff232e8c97238ca
Mohan0416 <manomohan417@gmail.com>
2025-03-10 20:20:52 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import tensorflow as tf
 import streamlit as st
+import gdown
 from PIL import Image
 
 st.set_page_config(page_title="Plant Disease Classifier", page_icon="🌿", layout="wide")
@@ -11,15 +12,24 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(working_dir, "Model", "plant_disease_prediction_model.h5")
 class_indices_path = os.path.join(working_dir, "Model", "class_indices.json")
 
+gdrive_url = "https://drive.google.com/file/d/161k3OqiMJjAP7__6x5eXzropqGCzlESs/view?usp=drive_link"
+
+if not os.path.exists(model_path):
+    st.info("Downloading model file...")
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    gdown.download(gdrive_url, model_path, quiet=False)
+
 @st.cache_resource
 def load_model():
     return tf.keras.models.load_model(model_path)
 
 model = load_model()
 
+# Load class indices
 with open(class_indices_path, "r") as f:
     class_indices = json.load(f)
 index_to_class = {int(k): v for k, v in class_indices.items()}
+
 
 def preprocess_image(image, target_size=(224, 224)):
     img = image.resize(target_size)
@@ -46,7 +56,10 @@ if uploaded_image is not None:
     col1, col2 = st.columns(2)
     
     with col1:
-        st.image(image, caption="Uploaded Image", use_container_width=True)
+        max_width = 400  # Adjust the width as needed
+        image.thumbnail((max_width, max_width))  # Resize the image
+        st.image(image, caption="Uploaded Image", use_container_width=False)
+
 
     with col2:
         if st.button("🔍 Predict"):
